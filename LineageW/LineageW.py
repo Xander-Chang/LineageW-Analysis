3e57bf3bb7581c3bcdd9a66ef4aba2284a7b1b94
Xander-Chang <103412302+Xander-Chang@users.noreply.github.com>
2023-01-05 17:28:01 +0800

    Add files via upload

--- LineageW/LineageW.py
+++ LineageW/LineageW.py
@@ -4,38 +4,44 @@ import numpy as np
 
 # 資料引入
 df = pd.read_excel("天堂w_練功.xlsx", sheet_name="66")
-print(df,'\n')
+# print('原本的樣子:\n', df,'\n')
 
 # 資料清洗
 df1 = df.iloc[:,0:4]
-print(df1,'\n')
+print('iloc 取前面5個 col:\n', df1,'\n')
+# df3 = df1.dropna(how='all')      
+# print('dropna 空格的資料\n', df3,'\n')
 df2 = df1.dropna(how='any')
-print(df2,'\n')
+# print('dropna 空格的資料\n', df2,'\n')
 
 
 col_names = ['等級', '地點']
 summeries = {'經驗值': 'mean', '錢': 'mean'}
-df2 = df1.groupby(by=col_names).agg(summeries).reset_index()
-#print(df2,'\n')
+df3 = df2.groupby(by=col_names).agg(summeries).reset_index()
+# print('groupby排序/計算後:\n', df3,'\n')
 
 
 
-'''
-level = df2["等級"]
-spot = df2["地點"].astype(str)
-ex = df2["經驗值"]
-money = df2["錢"]
+# 製圖
+level = df3["等級"]
+spot = df3["地點"].astype(str)
+ex = df3["經驗值"]
+money = df3["錢"]
 
 
 plt.figure(figsize=(16,8))
-plt.plot(spot, ex)
-plt.yticks(np.arange(0, 1, 0.1))
+plt.bar(spot, ex)
+plt.yticks(np.arange(0, 1, 0.05))
+plt.grid()
+plt.legend()
 
+
+for x,y in zip(df3["地點"], ex):
+    plt.text(x, y, ex)
 
 plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
 plt.rcParams['axes.unicode_minus'] = False
 
 plt.title("66等妖精練功地點效率圖")
 plt.show()
-'''
 
